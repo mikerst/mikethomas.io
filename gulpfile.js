10a9eb9f9da01729cd1e79fc1a7cce33c1c5e5cf
mikerst <mikerst@gmail.com>
2015-10-25 13:52:30 +0000

    setting up + basic gulp sass tasks

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+var sourcemaps      = require('gulp-sourcemaps');
+var webserver       = require('gulp-webserver');
+var autoprefixer    = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/**/*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('webserver', function() {
+  return gulp.src( '.' )
+    .pipe(webserver({
+      host:             'localhost',
+      port:             '1337',
+      livereload:       true,
+      directoryListing: false
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.{scss,sass}', ['sass']);
+})
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
+
