39fbc6a60d648010df78492a87eb31711c14c7c4
mikerst <mikerst@gmail.com>
2016-08-21 22:07:02 +0100

    Setting up gh-pages deploy via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp 			  = require('gulp');
 var server   		= require('gulp-webserver');
 var less 			  = require('gulp-less');
+var ghPages     = require('gulp-gh-pages');
 var path        = require("path");
 
 
 // SERVER
 gulp.task('server', function() {
-  gulp.src('./')
+  gulp.src('./dist/')
     .pipe(server({
       host: 'localhost',
       port: 8001,
@@ -28,5 +29,11 @@ gulp.task('less', function () {
 gulp.task('watch', function() {
 	gulp.watch('./less/**/*.less', ['less']);
 })
+
+// DEPLOY
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('default', [ 'watch', 'server' ]);
